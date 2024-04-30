33214a58daf43ef62d201435774f06dfb69bdd51
Eduardo C. Resplandes <resplandes930@gmail.com>
2024-04-30 17:07:35 -0300

    resolvendo conflito

--- routes/api.php
+++ routes/api.php
@@ -2,17 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
-
-Route::get('/user', function (Request $request) {
-    return 'teste';
-=======
 use App\Http\Controllers\AuthController;
 
 Route::prefix("/autenticacao")->group(function () {
-    Route::controller(AuthController::class)->group(function (){
+    Route::controller(AuthController::class)->group(function () {
         Route::post('/login', 'login');
-        Route::post('/logout','logout');
+        Route::post('/logout', 'logout');
     });
->>>>>>> configuracao_jwt
 });
