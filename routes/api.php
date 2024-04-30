ee0154f211670772a1e50d3cc5ae1191320d3de7
Danilo Ornelas do Nascimento <danilo.ornelas@gr.com.br>
2024-04-30 15:51:08 -0300

    Configuração do JWT

---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+Route::prefix("/autenticacao")->group(function () {
+    Route::controller(AuthController::class)->group(function (){
+        Route::post('/login', 'login');
+        Route::post('/logout','logout');
+    });
+});
