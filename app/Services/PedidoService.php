7eaf6348a2c4e69f34b530ed76c1c0d128507f5d
Eduardo C. Resplandes <resplandes930@gmail.com>
2024-07-03 15:08:03 -0300

    correcao

--- app/Services/PedidoService.php
+++ app/Services/PedidoService.php
@@ -248,7 +248,7 @@ class PedidoService
     public function listarMonica()
     {
         // 1ª Passo -> Buscar todos os pedidos com status 2
-        $query = PedidoResource::collection(Pedido::where('id_status', 2)->get());
+        $query = PedidoResource::collection(Pedido::whereIn('id_status', [2, 12])->get());
 
         // 2º Passo -> Retornar resposta
         if ($query) {
