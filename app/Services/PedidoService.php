88059942d4f8fa691646dad723f483230c800fc3
Eduardo C. Resplandes <resplandes930@gmail.com>
2024-08-01 15:23:54 -0300

    correcao dp

--- app/Services/PedidoService.php
+++ app/Services/PedidoService.php
@@ -43,6 +43,7 @@ class PedidoService
         $query = PedidoResource::collection(
             Pedido::orderBy('created_at', 'desc')
                 ->where('id_local', $id)
+                ->where('id_criador', '!=', '7')
                 ->take(500)
                 ->get()
         );
@@ -105,6 +106,7 @@ class PedidoService
         $query = PedidoResource::collection(
             Pedido::orderBy('created_at', 'desc')
                 ->whereIn('id_local', [2, 3]) // Busca pedidos com IDs 2 e 3
+                ->where('id_criador', '!=', '7')
                 ->get()
         );
 
