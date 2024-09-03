2828d1a85536ddd6699071bcca09b20b12d7d563
root <paulinho438@gmail.com>
2024-09-03 15:07:09 -0300

    Revert "fix: Data de aprovação pegando do campo errado"
    This reverts commit 1fedb36164ee0a0ac033a756b4871c68e6679fda.

--- app/Services/PedidoService.php
+++ app/Services/PedidoService.php
@@ -1354,7 +1354,7 @@ class PedidoService
 
             // Adicionar a data de aprovação ao recurso
             $pedido = $pedido->map(function ($item) use ($dtAprovacao) {
-                $item->dt_assinatura = $dtAprovacao;
+                $item->dt_aprovacao = $dtAprovacao;
                 return new PedidoAprovadoResource($item);
             });
 
