d0fa84ce77a0569a789ca397451a819ba829145d
Eduardo C. Resplandes <resplandes930@gmail.com>
2024-11-19 10:17:36 -0300

    fix: Correções

--- app/Services/PedidoService.php
+++ app/Services/PedidoService.php
@@ -166,7 +166,7 @@ class PedidoService
         // 1º Passo -> Buscar todos os pedidos cadastrados
         $query = Pedido::orderBy('created_at', 'desc')
             ->where('id_status', '!=', 8)
-            ->whereIn('id_criador', [7, 80]);
+            ->where('id_criador', '!=', 7);
 
         // Verifica se os campos existem e adiciona as cláusulas
         if ($request->has('numero_pedido')) {
@@ -226,7 +226,7 @@ class PedidoService
         $query = PedidoResource::collection(
             Pedido::where('id_status', 1)
                 ->where('id_link', 2)
-                ->whereIn('id_criador', [7, 80])
+                ->where('id_criador', '!=', 7)
                 ->orderBy('urgente', 'desc')
                 ->get()
         );
